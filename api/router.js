847150c8dea20a8850afbb3866b882734cc0789d
mlseligson <mseligson125@gmail.com>
2024-07-15 00:22:40 -0600

    Add error handling route

--- api/router.js
+++ api/router.js
@@ -2,9 +2,13 @@ import { Router } from 'express';
 import trips from './controllers/trips.js';
 import flights from './controllers/flights.js';
 
+import errorHandler from './error.js';
+
 const router = Router();
 
 router.use('/trips', trips);
 router.use('/flights', flights);
+
+router.use(errorHandler);
 
 export default router;
