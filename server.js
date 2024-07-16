a7785d271669a026cec084127205eb291efcb4ac
mlseligson <mseligson125@gmail.com>
2024-07-16 01:27:08 -0600

    CORS support

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import express, { Router } from 'express'
 import router from './api/router.js'
-import logger from 'morgan';
+import logger from 'morgan'
+import cors from 'cors'
 
 let app = express();
 
+app.use(cors());
 app.use(logger('tiny'));
 app.use(express.json());
 
-app.use(router);
+app.use('/api', router);
 
 app.get('/', (req, res) => {
   res.json({a: req.query.a})
-})
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000")
