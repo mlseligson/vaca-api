c53698e22cf9280c062d2cf429da274ed6cea796
mlseligson <mseligson125@gmail.com>
2024-09-05 21:15:37 -0600

    Add support for arrays in createActivity()

--- api/controllers/activities.js
+++ api/controllers/activities.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { text } from 'express'
 import { connectToPool } from '../db.js';
 
 const activitiesRouter = express.Router();
@@ -6,7 +6,7 @@ const activitiesRouter = express.Router();
 activitiesRouter.use(connectToPool);
 
 activitiesRouter.get('/', indexActivities);
-activitiesRouter.post('/', createActivity);
+activitiesRouter.post('/trip/:tripId', createActivity);
 activitiesRouter.get('/:id', getActivity);
 activitiesRouter.patch('/:id', updateActivity);
 activitiesRouter.delete('/:id', deleteActivity);
@@ -29,9 +29,17 @@ async function indexActivities(req, res, next) {
   }
 }
 
-async function createActivity(req, res, next) {
+function createActivity(req, res, next) {
+  const handler = (req.body.bulk) ?
+    createMultipleActivities : createSingleActivity;
+
+  handler.call(req, res, next);
+}
+
+async function createSingleActivity(req, res, next) {
   try {
-    const { name, description, location, image_url, start_time, end_time, trip_id } = req.body;
+    const { name, description, location, image_url, start_time, end_time } = req.body;
+    const { tripId: trip_id } = req.params;
 
     const newActivity = await req.client.query({
       text: 'INSERT INTO activities (name, description, location, image_url, start_time, end_time, trip_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
@@ -42,6 +50,27 @@ async function createActivity(req, res, next) {
       throw new Error({status: 404});
 
     res.status(201).json(newActivity.rows[0]);
+  } catch(err) {
+    next(err);
+  } finally {
+    req.client.release();
+  }
+}
+
+async function createMultipleActivities(req, res, next) {
+  try {
+    const { activities } = req.body;
+    const { tripId: trip_id } = req.params;
+
+    const newActivities = await req.client.query({
+      text: 'INSERT INTO activities (name, trip_id) SELECT name, trip_id FROM jsonb_to_recordset($1::jsonb) AS t (name text, trip_id integer)',
+      values: JSON.stringify(activities.map(name => {name, trip_id}))
+    });
+
+    if (!newActivities.rowCount)
+      throw new Error({status: 404});
+      
+    res.status(201).json({inserted: newActivities.rowCount});
   } catch(err) {
     next(err);
   } finally {
