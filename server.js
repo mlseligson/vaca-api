6712d33bf50c9c543dcfcd397ad5c36a171e0bf7
mlseligson <mseligson125@gmail.com>
2025-01-26 01:41:29 -0600

    Serve angular build from express (should be conditional on run configuration)

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express, { Router } from 'express'
+import express from 'express'
 import router from './api/router.js'
 import logger from 'morgan'
 import cors from 'cors'
+import path from 'node:path'
 
 let app = express();
 
@@ -10,6 +11,12 @@ app.use(logger('tiny'));
 app.use(express.json());
 
 app.use('/api', router);
+
+
+app.use('/', express.static(path.join(path.resolve(), 'frontend')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(path.resolve(), 'frontend/index.html'));
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000")
